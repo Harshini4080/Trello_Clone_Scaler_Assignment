62e70bb97af9c6f00de225bd1472aa29f7261dcc
Harshini Ramavath <harshiniramavath22@gmail.com>
2026-01-16 19:46:28 +0530

    Updated backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 // ---------------------- SERVER ----------------------
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
 // ---------------------- TEST DB ----------------------
 app.get("/", (req, res) => {
